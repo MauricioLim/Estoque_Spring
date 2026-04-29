399b30720c1c71cf7bd0049c1bd47742be6d5e4b
Mauricio Lima <mauriciodlima22@gmail.com>
2026-04-29 19:24:49 -0300

    Ajustando o CORS

--- src/main/java/com/estoque/config/CorsConfig.java
+++ src/main/java/com/estoque/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174", "https://*.vercel.app", "https://estoque-front-seven.vercel.app/")
+                        .allowedOrigins("http://localhost:5174", "https://*.vercel.app", "https://estoque-front-seven.vercel.app/register")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
