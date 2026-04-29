294bac21c4eb8e0a97834e653ddfc4af79a522be
Mauricio Lima <mauriciodlima22@gmail.com>
2026-04-29 19:46:01 -0300

    Add @CrossOrigin to AuthController for CORS

--- src/main/java/com/estoque/controller/AuthController.java
+++ src/main/java/com/estoque/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "https://estoque-front-seven.vercel.app", allowCredentials = "true")
 public class AuthController {
 
     private final UserRepository userRepository;
